cfd9256a682eb36f135e9a7fb87376308e368d34
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2025-08-08 13:15:06 -0700

    add db escapes

--- WikiNotification/listener.py
+++ WikiNotification/listener.py
@@ -206,12 +206,19 @@ class WikiNotificationChangeListener(Component):
         return return_sids
 
     def _watch_renamed_page(self, pagename, old_pagename):
-        self.log.info("UPDATE session_attribute SET value=value || %s WHERE name=%s AND value LIKE %s AND value NOT LIKE %s",
-                      f'{pagename},', 'watched_pages', f'%,{old_pagename},%', f'%,{pagename},%')
         with self.env.db_transaction as db:
             cursor = db.cursor()
-            cursor.execute("UPDATE session_attribute SET value=value || %s WHERE name=%s AND value LIKE %s AND value NOT LIKE %s",
-                           (f'{pagename},', 'watched_pages', f'%,{old_pagename},%', f'%,{pagename},%'))
+            q = """UPDATE session_attribute
+    SET value=value || %s
+    WHERE name=%s
+    AND value {like} %s
+    AND value NOT {like} %s;""".format(like=db.like())
+            self.log.info(q, f'{pagename},', 'watched_pages',
+                          db.like_escape(f'%,{old_pagename},%'),
+                          db.like_escape(f'%,{pagename},%'))
+            cursor.execute(q, (f'{pagename},', 'watched_pages',
+                               db.like_escape(f'%,{old_pagename},%'),
+                               db.like_escape(f'%,{pagename},%')))
 
 
 class WikiNotificationNotificationFormatter(Component):
